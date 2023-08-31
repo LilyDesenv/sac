c044fb5ac53479a8fe2a6a4303d803951893f2ca
LilyDesenv <alissandra.o.t@gmail.com>
2023-08-31 10:06:20 -0300

    ajuste nas classes de repositorys

--- src/main/java/com/sac/sac/repositories/AtendenteRepository.java
+++ src/main/java/com/sac/sac/repositories/AtendenteRepository.java
@@ -1,10 +1,13 @@
 package com.sac.sac.repositories;
 
 import com.sac.sac.domain.Atendente;
+import com.sac.sac.domain.TimeAtendimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AtendenteRepository extends JpaRepository<Atendente,Long> {
     Optional<Atendente> findAtendenteById(Long id);
+    List<Atendente> findAllByTimeAtendimento(TimeAtendimento timeAtendimento);
 }
--- src/main/java/com/sac/sac/repositories/SolicitacaoRepository.java
+++ src/main/java/com/sac/sac/repositories/SolicitacaoRepository.java
@@ -1,11 +1,18 @@
 package com.sac.sac.repositories;
 
+import com.sac.sac.domain.Cliente;
 import com.sac.sac.domain.Solicitacao;
+import com.sac.sac.domain.StatusAtendimento;
+import com.sac.sac.domain.Tiposolicitacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
     Optional<Solicitacao> findSolicitacaoById(Long id);
+    List<Solicitacao> findAllByStatusAtendimento(StatusAtendimento statusAtendimento);
+    List<Solicitacao> findAllByStatusAtendimentoAndAndTiposolicitacao(StatusAtendimento statusAtendimento, Tiposolicitacao tiposolicitacao);
+    List<Solicitacao> findAllByClienteAndTiposolicitacaoAndAndStatusAtendimento(Cliente cliente, Tiposolicitacao tiposolicitacao,StatusAtendimento statusAtendimento);
 
 }
